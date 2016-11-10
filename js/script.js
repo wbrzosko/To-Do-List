465a0e22fb4a46d61d27733b017681b73f21366f
wbrzosko <wbrzosko@gmail.com>
2016-11-10 19:46:24 +0100

    Primary

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 angular.module('app',['ngRoute']) 
     
+    .factory('Todos', function() {
+        return [
+        { name: 'AngularJS Directives', completed: true, note: 'add notes...' },
+        { name: 'Data binding', completed: true, note: 'add notes...' },
+        { name: '$scope', completed: true, note: 'add notes...' },
+        { name: 'Controllers and Modules', completed: true, note: 'add notes...' },
+        { name: 'Templates and routes', completed: true, note: 'add notes...' },
+        { name: 'Filters and Services', completed: false, note: 'add notes...' },
+        { name: 'Get started with Node/ExpressJS', completed: false, note: 'add notes...' },
+        { name: 'Setup MongoDB database', completed: false, note: 'add notes...' },
+        { name: 'Be awesome!', completed: false, note: 'add notes...' },
+    ];
+    })
+
     .controller('TodoController', ['$scope', 'Todos', function($scope, Todos) {
         $scope.todos = Todos;
     }])
 
-    .factory('Todos', function() {
-    return [
-            { name: 'Master HTML/CSS/Javascript', completed: true },
-            { name: 'Learn AngularJS', completed: false },
-            { name: 'Build NodeJS backend', completed: false },
-            { name: 'Get started with ExpressJS', completed: false },
-            { name: 'Setup MongoDB database', completed: false },
-            { name: 'Be awesome!', completed: false },
-        ];
-    })
+    .controller('ToDoDetailController', ['$scope', '$routeParams', 'Todos', function($scope, $routeParams, Todos) {
+        $scope.todo = Todos[$routeParams.id];
+    }])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider  
             .when('/', {
                 templateUrl: '/todos.html',
                 controller: 'TodoController'
+            })
+            .when('/:id', {
+                templateUrl: '/todoDetails.html',
+                controller: 'ToDoDetailController'    
             });
     }]);
